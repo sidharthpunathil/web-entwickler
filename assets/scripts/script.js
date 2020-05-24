aaeab78be66073267e864ddcdbe9d8ab2f73ecf3
sidharthpunathil <sidharthpunathil714@gmail.com>
2020-05-24 18:51:36 +0530

    initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,20 @@
+// window.addEventListener("scroll", function(){
+//     let nav = document.getElementById("nav")
+//     let logo = document.querySelector(".logo")
+//     if(window.pageYOffset >0){
+//         logo.style.height = "80PX";
+//         nav.classList.add("sticky")
+//     }else
+//     {   
+//         logo.style.height = "100px";
+//         nav.classList.remove("sticky")
+//     }
+
+// });
+
+
+
+// window.addEventListener("scroll", function(){
+//     let nav = document.querySelector("nav");
+//     nav.classList.toggle("sticky",window.scrollY >0)
+// }) 
